5b6d400430c99e26eba2c39d0c9d35212c6fbf10
MB-Teacher <MB-Teacher@MB-207-teacher>
2020-01-14 21:16:36 +0800

    jpa

--- src/main/java/com/web/mvc/entity/one2one/Test1.java
+++ src/main/java/com/web/mvc/entity/one2one/Test1.java
@@ -1,12 +1,14 @@
 package com.web.mvc.entity.one2one;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.web.mvc.entity.JPAUtil;
 import javax.persistence.EntityManager;
 
 public class Test1 {
     static EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-    public static void main(String[] args) {
-        add("老李");
+    public static void main(String[] args) throws Exception {
+        //add("老李");
+        get(1L);
     }
     
     public static void add(String name) {
@@ -16,5 +18,12 @@ public class Test1 {
         em.persist(person);
         em.getTransaction().commit();
         System.out.println("Add OK !");
+    }
+    
+    public static void get(Long id) throws Exception {
+        Person person = em.find(Person.class, id);
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(person);
+        System.out.println(json);
     }
 }
