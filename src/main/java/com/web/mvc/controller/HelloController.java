a1c1264cc674b02e52f0d903b3a4dba7181ba649
MB-Teacher <MB-Teacher@MB-207-teacher>
2020-01-14 19:47:06 +0800

    jpa

---
+++ src/main/java/com/web/mvc/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.web.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    
+    @GetMapping("/test")
+    public String test() {
+        return "Hello Test";
+    }
+    
+}
