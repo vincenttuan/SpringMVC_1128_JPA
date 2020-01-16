8a2605541dc305385f54382ed81ced4bfef98a21
MB-Teacher <MB-Teacher@MB-207-teacher>
2020-01-16 20:01:13 +0800

    jpa

--- src/main/java/com/web/mvc/entity/one2one/Husband.java
+++ src/main/java/com/web/mvc/entity/one2one/Husband.java
@@ -9,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -24,11 +25,17 @@ public class Husband {
     private String name;
     
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "wife_id")
+    //@JoinColumn(name = "wife_id")
     //可加入 SQL 外鍵約束 SQL
     //ALTER TABLE APP.HUSBAND ADD CONSTRAINT FK_WIFE FOREIGN KEY (WIFE_ID) REFERENCES APP.WIFE(ID);        
     //可移除 SQL 外鍵約束 SQL
-    //ALTER TABLE APP.HUSBAND DROP CONSTRAINT FK_WIFE;        
+    //ALTER TABLE APP.HUSBAND DROP CONSTRAINT FK_WIFE;
+    @JoinColumns( // 將 wife 資料加入 husband 中
+            {
+                @JoinColumn(name = "wife_id", referencedColumnName = "id"),
+                @JoinColumn(name = "wife_mame", referencedColumnName = "name")
+            }
+    )        
     Wife wife;
     
     public Long getId() {
