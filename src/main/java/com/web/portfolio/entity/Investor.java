96a23b5b7ea870556fe3e94611e5f8bea10fcdc6
MB-Teacher <MB-Teacher@MB-207-teacher>
2020-02-04 20:51:03 +0800

    jpa

---
+++ src/main/java/com/web/portfolio/entity/Investor.java
@@ -0,0 +1,102 @@
+package com.web.portfolio.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.io.Serializable;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Investor implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column
+    private String username;
+    
+    @Column
+    private String password;
+    
+    @Column
+    private String email;
+
+    @OneToMany(cascade=CascadeType.PERSIST, mappedBy="investor")
+    @JsonIgnoreProperties("investor")
+    private Set<Portfolio> portfolios;
+    
+    @OneToMany(cascade=CascadeType.PERSIST, mappedBy="investor")
+    @JsonIgnoreProperties("investor")
+    private Set<Watch> watchs;
+    
+    public Investor() {
+    }
+
+    public Investor(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Portfolio> getPortfolios() {
+        return portfolios;
+    }
+
+    public void setPortfolios(Set<Portfolio> portfolios) {
+        this.portfolios = portfolios;
+    }
+
+    public Set<Watch> getWatchs() {
+        return watchs;
+    }
+
+    public void setWatchs(Set<Watch> watchs) {
+        this.watchs = watchs;
+    }
+
+    @Override
+    public String toString() {
+        return "Investor{" + "id=" + id + ", username=" + username + ", password=" + password + ", email=" + email + '}';
+    }
+    
+    
+}
