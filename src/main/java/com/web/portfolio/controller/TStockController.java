9ee06fc118f97e52115b9dfeaacfb717dd171898
MB-Teacher <MB-Teacher@MB-207-teacher>
2020-02-11 18:36:19 +0800

    jpa

---
+++ src/main/java/com/web/portfolio/controller/TStockController.java
@@ -0,0 +1,92 @@
+package com.web.portfolio.controller;
+
+import com.web.portfolio.entity.Classify;
+import com.web.portfolio.entity.TStock;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+import yahoofinance.histquotes.HistoricalQuote;
+import yahoofinance.histquotes.Interval;
+
+@RestController
+@RequestMapping("/portfolio/tstock")
+public class TStockController {
+
+    @PersistenceContext
+    protected EntityManager em;
+
+    @PostMapping(value = {"/", "/add"})
+    @Transactional
+    public TStock add(@RequestBody Map<String, String> map) {
+        Classify classify = em.find(Classify.class, map.get("classify_id"));
+        TStock ts = new TStock();
+        ts.setName(map.get("name"));
+        ts.setSymbol(map.get("symbol"));
+        ts.setClassify(classify);
+        em.persist(ts);
+        em.flush(); // 取得最新 id
+        return ts;
+    }
+    
+    @PutMapping(value = {"/", "/update"})
+    @Transactional
+    public TStock update(@RequestBody Map<String, String> map) {
+        Classify classify = em.find(Classify.class, map.get("classify_id"));
+        TStock ts = em.find(TStock.class, map.get("id"));;
+        ts.setName(map.get("name"));
+        ts.setSymbol(map.get("symbol"));
+        ts.setClassify(classify);
+        em.persist(ts);
+        em.flush();
+        return ts;
+    }
+    
+    @DeleteMapping(value = {"/{id}", "/delete/{id}"})
+    @Transactional
+    public Boolean delete(@PathVariable("id") Long id) {
+        TStock tStock = em.find(TStock.class, id);
+        em.remove(tStock);
+        return true;
+    }
+    
+    @GetMapping(value = {"/{id}", "/get/{id}"})
+    @Transactional
+    public TStock get(@PathVariable("id") Long id) {
+        TStock tStock = em.find(TStock.class, id);
+        return tStock;
+    }
+
+    @GetMapping(value = {"/", "/query"})
+    public List<TStock> query() {
+        Query query = em.createQuery("select t from TStock t");
+        List<TStock> list = query.getResultList();
+        return list;
+    }
+
+    @GetMapping(value = {"/refresh"}) // 更新股價
+    @Transactional
+    public List<TStock> refresh() {
+        // 待撰寫
+        return null;
+    }
+
+    @GetMapping(value = {"/histquotes/{symbol}"}) // 歷史股價
+    public List<HistoricalQuote> queryHistQuotes(@PathVariable("symbol") String symbol) {
+        // 待撰寫
+        return null;
+    }
+}
